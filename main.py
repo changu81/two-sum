54c66bfab7a6b2fa0941b7fe17a6aca75e045dad
changu81 <ydi990801@gmail.com>
2023-03-09 19:53:57 +0900

    modify print->return lsit

--- main.py
+++ main.py
@@ -2,5 +2,4 @@ def twoSum(nums: List[int], target: int) -> List[int]:
     for i in range(0,len(nums)):
         for j in range (i+1,len(nums)):
             if nums[i]+nums[j] == target:
-                print(i, j)
-    return
+                return [i,j]
